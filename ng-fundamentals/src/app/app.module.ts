ac805ac0e0a4b400e65066a2aca9d4ced6dee853
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-04-30 16:21:40 +0300

    404 router added something wrong about it.

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -1,25 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/events-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
-
+import { RouterModule } from '@angular/router';
+import { appRoutes } from 'src/router';
+import { CreateEventComponent } from './create-event/create-event.component';
+import { Error404Component } from './errors/404.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent,
-    EventDetailsComponent
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
   ],
   providers: [],
   bootstrap: [EventsAppComponent]
--- ng-fundamentals/src/router.ts
+++ ng-fundamentals/src/router.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router'
 
 import { EventsListComponent } from './app/events/events-list.component';
 import { EventDetailsComponent } from './app/events/event-details/event-details.component';
+import { CreateEventComponent } from './app/create-event/create-event.component';
+import { Error404Component } from './app/errors/404.component';
+import { EventRouteActivator } from './app/events/event-details/event-route-activator.service';
 
 export const appRoutes:Routes = [
+    {path: 'events/new', component: CreateEventComponent},
     {path: 'events', component: EventsListComponent},
     {path: 'events/:id', component: EventDetailsComponent},
+    {path: '404', component: Error404Component, canActivate: [EventRouteActivator]},
     {path: '', redirectTo: '/events', pathMatch: 'full' }
 ]
